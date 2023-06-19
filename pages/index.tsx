1b8a2796c3ab3c3528639eb6821fcd90f0890a74
2019MM607 <jose.martinez19@catolica.edu.sv>
2023-06-19 14:46:34 -0600

    ADD(hero-module) hero screen added with a 3D animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,57 @@
+import React, { Suspense } from "react";
 import { Inter } from "next/font/google";
-import { Typography } from "@mui/material";
+import { Button, Container, Typography } from "@mui/material";
 import { MainLayout } from "@/components/layout/MainLayout";
+import Link from "next/link";
+
+const Spline = React.lazy(() => import("@splinetool/react-spline"));
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
-      <Typography variant="h1">Hello home</Typography>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Spline
+          scene="https://prod.spline.design/jvrtg5uqvdjWhYc2/scene.splinecode"
+          style={{ height: "100vh", width: "100vw" }}
+        />
+      </Suspense>
+      <Container maxWidth="sm">
+        <Typography
+          sx={{
+            fontFamily: inter,
+            fontSize: "2rem",
+            fontWeight: 700,
+            textAlign: "center",
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+          }}
+        >
+          Best NASA images from NASA-API
+        </Typography>
+
+        <Link href="/images">
+          <Button
+            color="primary"
+            size="large"
+            variant="contained"
+            sx={{
+              fontFamily: inter,
+              fontSize: "1rem",
+              fontWeight: 700,
+              position: "absolute",
+              top: "60%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+            }}
+          >
+            Get started
+          </Button>
+        </Link>
+      </Container>
     </>
   );
 }
